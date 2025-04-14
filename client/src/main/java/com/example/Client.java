d4e9018aff4ac6189bc8aecaf8dc530d33657a7e
Patri0224 <colombo.patrizio04@gmail.com>
2025-04-14 15:59:53 +0200

    javafx-fxml

--- client/src/main/java/com/example/Client.java
+++ client/src/main/java/com/example/Client.java
@@ -1,7 +1,11 @@
 package com.example;
 
-import java.io.*;
-import java.net.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
