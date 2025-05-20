bf0b058eca56c24df73c633136976429324d766f
Patri0224 <colombo.patrizio04@gmail.com>
2025-05-20 17:56:27 +0200

    scoperto errore RMI

--- client/src/main/java/bookRecommender/Client.java
+++ client/src/main/java/bookRecommender/Client.java
@@ -1,29 +1,37 @@
 package bookRecommender;
 
 
+import bookRecommender.entita.Libri;
 import bookRecommender.rmi.ServerBookRecommenderInterface;
 
-import java.io.*;
-import java.net.Socket;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
+import java.sql.SQLException;
+import java.util.Arrays;
 
-import static java.lang.System.exit;
 public class Client {
     static String hostName = "127.0.0.1";
     static int PORT = 10001;
+    static ServerBookRecommenderInterface stub;
+    static UtenteGestore utenteGestore;
 
-
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         try {
-            Registry registry=java.rmi.registry.LocateRegistry.getRegistry(hostName,PORT);
-            ServerBookRecommenderInterface stub = (ServerBookRecommenderInterface) registry.lookup("BookRecommender");
-            System.out.println(stub.login("admin", "admin"));
-            System.out.println(stub.login("admin", "admi"));
+            Registry registry = java.rmi.registry.LocateRegistry.getRegistry(hostName, PORT);
+            stub = (ServerBookRecommenderInterface) registry.lookup("BookRecommender");
+            Libri[] libri = stub.RicercaLibri("po","la",0);
+            for(Libri lib:libri){
+                System.out.println(lib.toString());
+            }
+            utenteGestore = new UtenteGestore(stub);
+
+
         } catch (RemoteException e) {
             throw new RuntimeException(e);
         } catch (NotBoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
