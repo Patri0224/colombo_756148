22f344f601776ccb0f3a93b5192a08a6e1d3cdbb
Patri0224 <colombo.patrizio04@gmail.com>
2025-05-23 19:48:26 +0200

    classi per andare indietro nella grafica

---
+++ client/src/main/java/graphics/comandoIndietro.java
@@ -0,0 +1,55 @@
+package graphics;
+
+import java.util.Stack;
+
+
+public class comandoIndietro {
+    private static BookRecommender gui;
+    private static Stack<oggettoIndietro> pila = new Stack<>();
+    public static final String PAGINA_HOME = "Pagina Home";
+    public static final String PAGINA_LIBRERIA = "Pagina Libreria";
+    public static final String PAGINA_LIBRO = "Pagina Libro";
+    public static final String PAGINA_AGGIUNGI_VALUTAZIONE = "Pagina Aggiungi Valutazione";
+    public static final String PAGINA_AUTENTICAZIONE = "Pagina Autenticazione";
+
+    private static void setGui(BookRecommender bookRecommender) {
+        gui = bookRecommender;
+        pila.clear();
+        pila.push(new oggettoIndietro(PAGINA_HOME, ""));
+    }
+
+    public static void indietro() {
+        if (pila.size() > 1) {
+            oggettoIndietro oggetto = pila.pop();
+            azioneIndietro(oggetto);
+        }
+    }
+
+    public static void aggiungiOggetto(oggettoIndietro oggetto) {
+        pila.push(oggetto);
+    }
+
+
+    private static void azioneIndietro(oggettoIndietro oggetto) {
+        switch (oggetto.getPagina()) {
+            case PAGINA_HOME -> {
+
+            }
+            case PAGINA_LIBRERIA -> {
+
+            }
+            case PAGINA_LIBRO -> {
+
+            }
+            case PAGINA_AGGIUNGI_VALUTAZIONE -> {
+
+            }
+            case PAGINA_AUTENTICAZIONE -> {
+
+            }
+        }
+
+    }
+
+
+}
---
+++ client/src/main/java/graphics/oggettoIndietro.java
@@ -0,0 +1,17 @@
+package graphics;
+
+public class oggettoIndietro {
+    private String pagina;
+    private String opzione;
+
+    public oggettoIndietro(String pagina, String opzione) {
+        this.pagina = pagina;
+        this.opzione = opzione;
+    }
+    public String getPagina() {
+        return pagina;
+    }
+    public String getOpzione() {
+        return opzione;
+    }
+}
